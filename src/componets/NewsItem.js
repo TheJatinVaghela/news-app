5520b8bcded0c191f5705864e6ef60897c3864e1
Jatin <thejatinvaghela@gmail.com>
2022-08-30 16:13:51 +0530

    card works

--- src/componets/NewsItem.js
+++ src/componets/NewsItem.js
@@ -4,22 +4,19 @@ import PropTypes from 'prop-types'
 export default class NewsItem extends Component {
     
     
-    h1="News-Item-Conmponets";
 
-    
-  
-  render() {
+     render() {
  
-     let {Info_OBJ}=this.props;
+     let {Title,Description,Text,Link,Img,Uniq_Id}=this.props;
 
       return (
       <>
-      <div className="card w-72">
-            <img src="..." className="card-img-top" alt="..."/>
+      <div className="card w-full Font">
+            <img src={Img} className="card-img-top" alt="..."/>
         <div className="card-body">
-            <h5 className="card-title">{Info_OBJ.Title}</h5>
-                <p className="card-text">{Info_OBJ.Description}</p>
-            <a href={Info_OBJ.Info.Link} target="/" className="btn btn-primary">{Info_OBJ.Info.Text}</a>
+            <h5 className="card-title font-bold">{Title.slice(0, 40)} <span  onClick={(e)=>{e.target.innerText=Title.slice(40,100)}}>...</span> </h5>
+                <p className="card-text">{Description.slice(0, 88)} <span  onClick={(e)=>{e.target.innerText=Description.slice(88,200)}}>...</span> </p>
+            <a href={Link} target="/" className="btn btn-primary Font mt-4">{Text}</a>
         </div>
     </div>
        
