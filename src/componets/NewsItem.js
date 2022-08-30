5cc71ce8d9471a07ba878bd9de4775ccda807b8d
Jatin <thejatinvaghela@gmail.com>
2022-08-30 12:02:37 +0530

    Api Info JSON

--- src/componets/NewsItem.js
+++ src/componets/NewsItem.js
@@ -14,9 +14,7 @@ export default class NewsItem extends Component {
     return (
       <>
         <h1>{this.h1}</h1>
-        <h1>{this.props.pr.namer} PR-1</h1>
-        <h1>{this.props.pr2.namer} PR-2</h1>
-        <h1>{this.props.pr3.namer} PR-3</h1>
+       
       </>
     )
   }
