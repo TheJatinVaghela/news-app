b6838c5525b8d43dffd1d0dac6f4f367190617d6
Jatin <thejatinvaghela@gmail.com>
2022-08-30 12:58:44 +0530

    How t USe props In class Its Awosome

--- src/componets/NewsItem.js
+++ src/componets/NewsItem.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class NewsItem extends Component {
-    constructor(){
-        super();
-        this.h1="News-Item-Conmponets";
-    }
-  static propTypes = {
-    prop: PropTypes
-  }
+    
+    
+    h1="News-Item-Conmponets";
 
+    
+  
   render() {
-    return (
+ 
+     let {Info_OBJ}=this.props;
+
+      return (
       <>
-        <h1>{this.h1}</h1>
+      <div className="card w-72">
+            <img src="..." className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <h5 className="card-title">{Info_OBJ.Title}</h5>
+                <p className="card-text">{Info_OBJ.Description}</p>
+            <a href={Info_OBJ.Info.Link} target="/" className="btn btn-primary">{Info_OBJ.Info.Text}</a>
+        </div>
+    </div>
        
       </>
     )
